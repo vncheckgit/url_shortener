b38fa6b55b3c779ad263009c6dc752174953c590
Nguyễn Tuấn Hiệp <nguyentuanhiep01@gmail.com>
2024-05-31 09:05:17 +0700

    Update settings.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -49,7 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ROOT_URLCONF = 'url_shortener.urls'
 
