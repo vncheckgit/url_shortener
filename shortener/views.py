bfa5bd6f3c242f85f69a7df88560de8528dd93bd
Nguyễn Tuấn Hiệp <nguyentuanhiep01@gmail.com>
2024-05-31 09:08:01 +0700

    Update views.py

--- shortener/views.py
+++ shortener/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, get_object_or_404
 from .models import URL
 
 
-def redirect_url(short_code):
+def redirect_url(request, short_code):
     url = get_object_or_404(URL, short_code=short_code)
     url.click_count += 1
     url.save()
