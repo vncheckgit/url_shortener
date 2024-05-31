269f0c48d22e844edb09ab237f7228476be49d6e
Nguyễn Tuấn Hiệp <nguyentuanhiep01@gmail.com>
2024-05-31 08:58:52 +0700

    Update views.py

--- shortener/views.py
+++ shortener/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import redirect, get_object_or_404
 from .models import URL
 
 
-def redirect_url( short_code):
+def redirect_url(short_code):
     url = get_object_or_404(URL, short_code=short_code)
     url.click_count += 1
     url.save()
     return redirect(url.original_url)
 
 
-def redirect_to_admin():
+def redirect_to_admin(request):
     return redirect('admin:index')
