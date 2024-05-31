19d95335b9dd80b79f83f66c4b88a17b1b1716a4
Nguyễn Tuấn Hiệp <nguyentuanhiep01@gmail.com>
2024-05-31 14:49:13 +0700

    Update urls.py

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -6,5 +6,4 @@ from shortener.views import redirect_to_admin
 urlpatterns = [
     path('admin', admin.site.urls),
     path('<str:short_code>/', views.redirect_url, name='redirect_url'),
-    path('', redirect_to_admin, name='redirect_to_admin'),
 ]
